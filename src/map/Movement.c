f2be9da02aa006adea2b6f7412a13374744bc0f5
anramire <anramire@student.42malaga.com>
2023-03-13 22:05:20 +0100

    Making vertical fix in diagonal collisions

--- src/map/Movement.c
+++ src/map/Movement.c
@@ -6,7 +6,7 @@
 /*   By: anramire <anramire@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 22:56:29 by anramire          #+#    #+#             */
-/*   Updated: 2023/03/13 20:45:44 by anramire         ###   ########.fr       */
+/*   Updated: 2023/03/13 22:01:30 by anramire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,12 @@ void	check_collision(t_map *map, t_player *player, int advance_x, int advance_y)
 static void	check_horizontal_collision(t_map *map, t_player *player, int advance_x);
 static void	check_vertical_collision(t_map *map, t_player *player, int advance_y);
 static void check_diagonal_collision(t_map *map, t_player *player, int advance_x, int advance_y);
+
+
+int check_left_up_player(t_player *player, t_map *map, int advance_x, int advance_y);
+int check_right_up_player(t_player *player, t_map *map, int advance_x, int advance_y);
+int check_left_down_player(t_player *player, t_map *map, int advance_x, int advance_y);
+int check_right_down_player(t_player *player, t_map *map, int advance_x, int advance_y);
 
 //Function to move player in the direction of direction vector
 void player_advance(t_map *map, t_player *player, int direction)
@@ -109,7 +115,113 @@ static void	check_horizontal_collision(t_map *map, t_player *player, int advance
 
 static void check_diagonal_collision(t_map *map, t_player *player, int advance_x, int advance_y)
 {
+	int aux_advance_y;
+
 	printf("Diagonal\n");
-	check_vertical_collision(map, player, advance_y);
-	check_horizontal_collision(map, player, advance_x);
+	if(advance_x < 0 && advance_y < 0)
+	{
+		printf("Arriba izquierda\n");
+		if(check_left_up_player(player, map, advance_x, advance_y) == 0 && 
+				check_right_up_player(player, map, advance_x, advance_y) == 0)
+			player->pos_y += advance_y;
+		else
+		{		
+			aux_advance_y = -1 * ((player->pos_y - (player->tam / 2 )) % map->height);
+			player->pos_y += aux_advance_y;
+		}
+	}
+	else if(advance_x > 0 && advance_y < 0)
+	{
+		printf("Arriba derecha\n");
+		if(check_left_up_player(player, map, advance_x, advance_y) == 0 && 
+				check_right_up_player(player, map, advance_x, advance_y) == 0)
+			player->pos_y += advance_y;
+		else
+		{		
+			aux_advance_y = -1 * ((player->pos_y - (player->tam / 2 )) % map->height);
+			player->pos_y += aux_advance_y;
+		}
+	}
+	else if(advance_x < 0 && advance_y > 0)
+	{
+		printf("Abajo izquierda\n");
+
+		if(check_left_down_player(player, map, advance_x, advance_y) == 0 && 
+				check_right_down_player(player, map, advance_x, advance_y) == 0)
+			player->pos_y += advance_y;
+		else
+		{
+			aux_advance_y = map->height - player->tam - ((player->pos_y - (player->tam / 2 )) % map->height);
+			player->pos_y += aux_advance_y;
+		}
+	}
+	else if(advance_x > 0 && advance_y > 0)
+	{
+		printf("Abajo derecha\n");
+		if(check_left_down_player(player, map, advance_x, advance_y) == 0 && 
+				check_right_down_player(player, map, advance_x, advance_y) == 0)
+			player->pos_y += advance_y;
+		else
+		{	
+			aux_advance_y = map->height - player->tam - ((player->pos_y - (player->tam / 2 )) % map->height);
+			player->pos_y += aux_advance_y;
+		}
+
+	}
+	player->pos_x += advance_x;
+}
+
+int check_left_up_player(t_player *player, t_map *map, int advance_x, int advance_y)
+{
+	int aux_x;
+	int aux_y;
+
+	aux_x = player->pos_x + advance_x - (player->tam / 2);
+	aux_y = player->pos_y + advance_y - (player->tam / 2);
+
+	if(map->map[aux_y / map->height][aux_x / map->width] == '1')
+		return -1;
+	return 0;
+}
+
+int check_right_up_player(t_player *player, t_map *map, int advance_x, int advance_y)
+{
+
+	int aux_x;
+	int aux_y;
+
+	aux_x = player->pos_x + advance_x + (player->tam / 2);
+	aux_y = player->pos_y + advance_y - (player->tam / 2);
+
+	if(map->map[aux_y / map->height][aux_x / map->width] == '1')
+		return -1;
+	return 0;
+}
+
+
+int check_left_down_player(t_player *player, t_map *map, int advance_x, int advance_y)
+{
+
+	int aux_x;
+	int aux_y;
+
+	aux_x = player->pos_x + advance_x - (player->tam / 2);
+	aux_y = player->pos_y + advance_y + (player->tam / 2);
+
+	if(map->map[aux_y / map->height][aux_x / map->width] == '1')
+		return -1;
+	return 0;
+}
+int check_right_down_player(t_player *player, t_map *map, int advance_x, int advance_y)
+{
+
+	int aux_x;
+	int aux_y;
+
+	aux_x = player->pos_x + advance_x + (player->tam / 2);
+	aux_y = player->pos_y + advance_y + (player->tam / 2);
+
+	if(map->map[aux_y / map->height][aux_x / map->width] == '1')
+		return -1;
+	return 0;
 }
