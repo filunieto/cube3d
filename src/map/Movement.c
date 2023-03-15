982db5df07b1e7617bceece916a1dc01b987ee72
anramire <anramire@student.42malaga.com>
2023-03-15 23:28:46 +0100

    Norminette passed provisional

--- src/map/Movement.c
+++ src/map/Movement.c
@@ -6,37 +6,41 @@
 /*   By: anramire <anramire@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 22:56:29 by anramire          #+#    #+#             */
-/*   Updated: 2023/03/15 22:31:56 by anramire         ###   ########.fr       */
+/*   Updated: 2023/03/15 23:06:39 by anramire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/map/Map.h"
 
-static void	check_collision(t_map *map, t_player *player, int advance_x, int advance_y);
-static void	check_horizontal_collision(t_map *map, t_player *player, int advance_x);
-static void	check_vertical_collision(t_map *map, t_player *player, int advance_y);
-static void check_diagonal_collision(t_map *map, t_player *player, int advance_x, int advance_y);
+//Functions of this file
+static void	check_collision(t_map *map, t_player *player,
+				int advance_x, int advance_y);
+static void	check_horizontal_collision(t_map *map, t_player *player,
+				int advance_x);
+static void	check_vertical_collision(t_map *map, t_player *player,
+				int advance_y);
+static void	check_diagonal_collision(t_map *map, t_player *player,
+				int advance_x, int advance_y);
 
 //Functions defined in Checking_Collisions_Sides.c
-extern int check_up_collisions(t_map *map, t_player *player, int advance_x, int advance_y);
-extern int	check_down_collisions(t_map *map, t_player *player, int advance_x, int advance_y);
-extern int	check_left_collisions(t_map *map, t_player *player, int advance_x, int advance_y);
-extern int	check_right_collisions(t_map *map, t_player *player, int advance_x, int advance_y);
-
-//This functions are defined in Utils_Collisions.c
-extern int check_left_up_player(t_player *player, t_map *map, int advance_x, int advance_y);
-extern int check_right_up_player(t_player *player, t_map *map, int advance_x, int advance_y);
-extern int check_left_down_player(t_player *player, t_map *map, int advance_x, int advance_y);
-extern int check_right_down_player(t_player *player, t_map *map, int advance_x, int advance_y);
+extern int	check_up_collisions(t_map *map, t_player *player,
+				int advance_x, int advance_y);
+extern int	check_down_collisions(t_map *map, t_player *player,
+				int advance_x, int advance_y);
+extern int	check_left_collisions(t_map *map, t_player *player,
+				int advance_x, int advance_y);
+extern int	check_right_collisions(t_map *map, t_player *player,
+				int advance_x, int advance_y);
 
 //Function to move player in the direction of direction vector
-void player_advance(t_map *map, t_player *player, int direction)
+void	player_advance(t_map *map, t_player *player, int direction)
 {
-	float angle;
-	float advance_x;
-	float advance_y;
+	float	angle;
+	float	advance_x;
+	float	advance_y;
+
 	angle = (player->angle * M_PI) / 180;
-	if(direction == -1 || direction == 1)
+	if (direction == -1 || direction == 1)
 	{
 		advance_x = (float)direction * player->vel * cos(angle);
 		advance_y = (float)direction * player->vel * sin(angle);
@@ -44,25 +48,24 @@ void player_advance(t_map *map, t_player *player, int direction)
 	}
 }
 
-
 //Function to check collision
-static void	check_collision(t_map *map, t_player *player, int advance_x, int advance_y)
+static void	check_collision(t_map *map, t_player *player,
+			int advance_x, int advance_y)
 {
-	
-	if(advance_y !=  0 && advance_x == 0)
+	if (advance_y != 0 && advance_x == 0)
 		check_vertical_collision(map, player, advance_y);
-	else if(advance_x != 0 && advance_y == 0)
+	else if (advance_x != 0 && advance_y == 0)
 		check_horizontal_collision(map, player, advance_x);
-	else if(advance_x != 0 && advance_y != 0)
+	else if (advance_x != 0 && advance_y != 0)
 		check_diagonal_collision(map, player, advance_x, advance_y);
 	printf("Player pos=> x: %d, y: %d\n", player->pos_x, player->pos_y);
-
 }
 
 //Function to check vertical collisions with walls
-static void	check_vertical_collision(t_map *map, t_player *player, int advance_y)
+static void	check_vertical_collision(t_map *map, t_player *player,
+			int advance_y)
 {
-	int aux_y;
+	int	aux_y;
 
 	if (advance_y < 0)
 		aux_y = check_up_collisions(map, player, 0, advance_y);
@@ -71,241 +74,40 @@ static void	check_vertical_collision(t_map *map, t_player *player, int advance_y
 	player->pos_y = aux_y;
 }
 
-
-static void	check_horizontal_collision(t_map *map, t_player *player, int advance_x)
+static void	check_horizontal_collision(t_map *map, t_player *player,
+			int advance_x)
 {	
 	int	aux_x;
-	
-	if(advance_x < 0)
+
+	if (advance_x < 0)
 		aux_x = check_left_collisions(map, player, advance_x, 0);
 	else
 		aux_x = check_right_collisions(map, player, advance_x, 0);
-	player->pos_x = aux_x;	
-
+	player->pos_x = aux_x;
 }
 
-static void check_diagonal_collision(t_map *map, t_player *player, int advance_x, int advance_y)
+//Function to check diagonal collisions with walls
+static void	check_diagonal_collision(t_map *map, t_player *plyr,
+			int advance_x, int advance_y)
 {
-	int aux_advance_y;
-	int aux_advance_x;
-	int aux_x;
-	int aux_y;
-	
-	aux_x = player->pos_x;
-	aux_y = player->pos_y;
-	printf("Diagonal\n");
-	if(advance_x < 0 && advance_y < 0)
+	if (advance_x < 0 && advance_y < 0)
 	{
-		printf("Arriba izquierda\n");
-		if(check_right_up_player(player, map, advance_x, advance_y) == 0 &&
-			check_left_up_player(player, map, advance_x, advance_y) == 0)
-		{	
-			aux_y += advance_y;
-		}
-		else
-		{
-			aux_advance_y = ((player->pos_y - (player->tam / 2 )) % map->height);
-			printf("Ceil: %d\n",((player->pos_y - (player->tam / 2 )) % map->height) );
-			if(aux_advance_y <= (int)(player->tam / 2) && aux_advance_y <= (-1 * advance_y)
-					&& aux_advance_y != 0)
-			{
-				printf("Entrando\n");
-				aux_y -= aux_advance_y;
-			}
-			else if(check_right_up_player(player, map, 0, advance_y) == 0 && 
-					check_left_up_player(player, map, 0, advance_y) == 0)
-			{
-				printf("New advance_y: %d\n", advance_y);
-				aux_y += advance_y;	
-			}
-			printf("Down alternative aux_advance_y: %d, and advance_y: %d, player_tam: %d\n", aux_advance_y, advance_y, (int)(player->tam / 2));
-
-		}
-
-		//----------
-		if(check_left_down_player(player, map, advance_x, advance_y) == 0 &&
-			check_left_up_player(player, map, advance_x, advance_y) == 0)
-		{	
-			aux_x += advance_x;
-		}
-		else
-		{
-			aux_advance_x = ((player->pos_x - (player->tam / 2)) % map->width);
-			if(aux_advance_x <= (int)(player->tam / 2) && aux_advance_x <= (-1 * advance_x)
-					&& aux_advance_x != 0)
-			{
-				printf("Entrando\n");
-				aux_x -= aux_advance_x;
-			}
-			else if(check_left_down_player(player, map, advance_x, 0) == 0 && 
-					check_left_up_player(player, map, advance_x, 0) == 0)
-			{
-				printf("New advance_x: %d\n", advance_x);
-				aux_x += advance_x;	
-			}
-			printf("Down alternative aux_advance_x: %d, and advance_x: %d, player_tam: %d\n", aux_advance_x, advance_x, (int)(player->tam / 2));
-		}
+		plyr->pos_y = check_up_collisions(map, plyr, advance_x, advance_y);
+		plyr->pos_x = check_left_collisions(map, plyr, advance_x, advance_y);
 	}
-	else if(advance_x > 0 && advance_y < 0)
+	else if (advance_x > 0 && advance_y < 0)
 	{
-		printf("Arriba derecha\n");
-		if(check_right_up_player(player, map, advance_x, advance_y) == 0 &&
-			check_left_up_player(player, map, advance_x, advance_y) == 0)
-		{	
-			aux_y += advance_y;
-		}
-		else
-		{
-			printf("Right: %d\n",check_right_up_player(player, map, 0, advance_y));
-			printf("Left: %d\n",check_left_up_player(player, map, 0, advance_y));
-			aux_advance_y = ((player->pos_y - (player->tam / 2 )) % map->height);
-			if(aux_advance_y <= (int)(player->tam / 2) && aux_advance_y <= (-1 * advance_y)
-					&& aux_advance_y != 0)
-			{
-				printf("Entrando\n");
-				aux_y -= aux_advance_y;
-			}
-			else if(check_right_up_player(player, map, 0, advance_y) == 0 && 
-					check_left_up_player(player, map, 0, advance_y) == 0)
-			{
-				printf("New advance_y: %d\n", advance_y);
-				aux_y += advance_y;	
-			}
-			printf("Down alternative aux_advance_y: %d, and advance_y: %d, player_tam: %d\n", aux_advance_y, advance_y, (int)(player->tam / 2));
-		}
-	//-------------	
-		if(check_right_down_player(player, map, advance_x, advance_y) == 0 &&
-			check_right_up_player(player, map, advance_x, advance_y) == 0)
-		{
-			aux_x += advance_x;
-		}
-		else
-		{
-
-			aux_advance_x = map->width - ((player->pos_x + (player->tam / 2)) % map->width);
-			if(aux_advance_x <= (int)(player->tam / 2) && aux_advance_x <= advance_x 
-					&& aux_advance_x != 0)
-			{
-				printf("Entrando\n");
-				aux_x += aux_advance_x;
-			}
-			else if(check_right_down_player(player, map, advance_x, 0) == 0 && 
-					check_left_down_player(player, map, advance_x, 0) == 0)
-			{
-				printf("New advance_x: %d\n", advance_x);
-				aux_x += advance_x;	
-			}
-		}
-
+		plyr->pos_y = check_up_collisions(map, plyr, advance_x, advance_y);
+		plyr->pos_x = check_right_collisions(map, plyr, advance_x, advance_y);
 	}
-	else if(advance_x < 0 && advance_y > 0)
+	else if (advance_x < 0 && advance_y > 0)
 	{
-		printf("Abajo izquierda\n");
-		if(check_right_down_player(player, map, advance_x, advance_y) == 0 &&
-			check_left_down_player(player, map, advance_x, advance_y) == 0)
-		{	
-			aux_y += advance_y;
-		}
-		else
-		{
-			aux_advance_y = map->height - ((player->pos_y + (player->tam / 2)) % map->height);
-			if(aux_advance_y <= (int)(player->tam / 2) && aux_advance_y <= advance_y
-					&& aux_advance_y != 0)
-			{
-				printf("Entrando\n");
-				aux_y += aux_advance_y;
-			}
-			else if(check_right_down_player(player, map, 0, advance_y) == 0 && 
-					check_left_down_player(player, map, 0, advance_y) == 0)
-			{
-				printf("New advance_y: %d\n", advance_y);
-				aux_y += advance_y;	
-			}
-		}
-
-		//-------------
-		if(check_left_down_player(player, map, advance_x, advance_y) == 0 &&
-			check_left_up_player(player, map, advance_x, advance_y) == 0)
-		{	
-			aux_x += advance_x;
-		}
-		else
-		{
-			aux_advance_x = ((player->pos_x - (player->tam / 2)) % map->width);
-			if(aux_advance_x <= (int)(player->tam / 2) && aux_advance_x <= (-1 * advance_x)
-					&& aux_advance_x != 0)
-			{
-				printf("Entrando\n");
-				aux_x -= aux_advance_x;
-			}
-			else if(check_left_down_player(player, map, advance_x, 0) == 0 && 
-					check_left_up_player(player, map, advance_x, 0) == 0)
-			{
-				printf("New advance_x: %d\n", advance_x);
-				aux_x += advance_x;	
-			}
-			printf("Down alternative aux_advance_x: %d, and advance_x: %d, player_tam: %d\n", aux_advance_x, advance_x, (int)(player->tam / 2));
-		}
-
-
+		plyr->pos_y = check_down_collisions(map, plyr, advance_x, advance_y);
+		plyr->pos_x = check_left_collisions(map, plyr, advance_x, advance_y);
 	}
-	else if(advance_x > 0 && advance_y > 0)
+	else if (advance_x > 0 && advance_y > 0)
 	{
-		printf("Abajo derecha\n");
-
-		if(check_right_down_player(player, map, advance_x, advance_y) == 0 &&
-			check_left_down_player(player, map, advance_x, advance_y) == 0)
-		{	
-			aux_y += advance_y;
-		}
-		else
-		{
-			printf("Right: %d\n",check_right_down_player(player, map, 0, advance_y));
-			printf("Left: %d\n",check_left_down_player(player, map, 0, advance_y));
-			aux_advance_y = map->height - ((player->pos_y + (player->tam / 2)) % map->height);
-			if(aux_advance_y <= (int)(player->tam / 2) && aux_advance_y <= advance_y
-					&& aux_advance_y != 0)
-			{
-				printf("Entrando\n");
-				aux_y += aux_advance_y;
-			}
-			else if(check_right_down_player(player, map, 0, advance_y) == 0 && 
-					check_left_down_player(player, map, 0, advance_y) == 0)
-			{
-				printf("New advance_y: %d\n", advance_y);
-				aux_y += advance_y;	
-			}
-
-			printf("Down alternative aux_advance_y: %d, and advance_y: %d, player_tam: %d\n", aux_advance_y, advance_y, (int)(player->tam / 2));
-		}
-	
-		if(check_right_down_player(player, map, advance_x, advance_y) == 0 &&
-			check_right_up_player(player, map, advance_x, advance_y) == 0)
-		{
-			aux_x += advance_x;
-		}
-		else
-		{
-			aux_advance_x = map->width - ((player->pos_x + (player->tam / 2)) % map->width);
-			if(aux_advance_x <= (int)(player->tam / 2) && aux_advance_x <= advance_x
-					&& aux_advance_x != 0)
-			{
-				printf("Entrando\n");
-				aux_x += aux_advance_x;
-			}
-			else if(check_right_down_player(player, map, advance_x, 0) == 0 && 
-					check_left_down_player(player, map, advance_x, 0) == 0)
-			{
-				printf("New advance_x: %d\n", advance_x);
-				aux_x += advance_x;	
-			}
-	printf("Down alternative aux_advance_x: %d, and advance_x: %d, player_tam: %d\n", aux_advance_x, advance_x, (int)(player->tam / 2));
-
-		}
-
+		plyr->pos_y = check_down_collisions(map, plyr, advance_x, advance_y);
+		plyr->pos_x = check_right_collisions(map, plyr, advance_x, advance_y);
 	}
-	player->pos_x = aux_x;
-	player->pos_y = aux_y;
-	printf("Player pos=> x: %d, y: %d\n", player->pos_x, player->pos_y);
-
 }
--- src/map/Utils_Collisions.c
+++ src/map/Utils_Collisions.c
@@ -6,63 +6,76 @@
 /*   By: anramire <anramire@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/15 21:40:39 by anramire          #+#    #+#             */
-/*   Updated: 2023/03/15 21:42:20 by anramire         ###   ########.fr       */
+/*   Updated: 2023/03/15 23:21:44 by anramire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/map/Map.h"
 
-int check_left_up_player(t_player *player, t_map *map, int advance_x, int advance_y)
+/*
+ * This function will check if left up corner would be inside a wall with the
+ * given advance_x and advance_y
+ * */
+int	check_left_up_player(t_player *player, t_map *map,
+			int advance_x, int advance_y)
 {
-	int aux_x;
-	int aux_y;
+	int	aux_x;
+	int	aux_y;
 
 	aux_x = player->pos_x + advance_x - (player->tam / 2) + 1 ;
 	aux_y = player->pos_y + advance_y - (player->tam / 2);
-
-	if(map->map[aux_y / map->height][aux_x / map->width] == '1')
-		return -1;
-	return 0;
+	if (map->map[aux_y / map->height][aux_x / map->width] == '1')
+		return (-1);
+	return (0);
 }
 
-int check_right_up_player(t_player *player, t_map *map, int advance_x, int advance_y)
+/*
+ * This function will check if right up corner would be inside a wall with the
+ * given advance_x and advance_y
+ * */
+int	check_right_up_player(t_player *player, t_map *map,
+			int advance_x, int advance_y)
 {
-
-	int aux_x;
-	int aux_y;
+	int	aux_x;
+	int	aux_y;
 
 	aux_x = player->pos_x + advance_x + (player->tam / 2) - 1;
 	aux_y = player->pos_y + advance_y - (player->tam / 2) + 1;
-
-	if(map->map[aux_y / map->height][aux_x / map->width] == '1')
-		return -1;
-	return 0;
+	if (map->map[aux_y / map->height][aux_x / map->width] == '1')
+		return (-1);
+	return (0);
 }
 
-
-int check_left_down_player(t_player *player, t_map *map, int advance_x, int advance_y)
+/*
+ * This function will check if left down corner would be inside a wall with the
+ * given advance_x and advance_y
+ * */
+int	check_left_down_player(t_player *player, t_map *map,
+			int advance_x, int advance_y)
 {
-
-	int aux_x;
-	int aux_y;
+	int	aux_x;
+	int	aux_y;
 
 	aux_x = player->pos_x + advance_x - (player->tam / 2) + 1;
 	aux_y = player->pos_y + advance_y + (player->tam / 2) - 1;
-
-	if(map->map[aux_y / map->height][aux_x / map->width] == '1')
-		return -1;
-	return 0;
+	if (map->map[aux_y / map->height][aux_x / map->width] == '1')
+		return (-1);
+	return (0);
 }
-int check_right_down_player(t_player *player, t_map *map, int advance_x, int advance_y)
-{
 
-	int aux_x;
-	int aux_y;
+/*
+ * This function will check if right down corner would be inside a wall with the
+ * given advance_x and advance_y
+ * */
+int	check_right_down_player(t_player *player, t_map *map,
+			int advance_x, int advance_y)
+{
+	int	aux_x;
+	int	aux_y;
 
 	aux_x = player->pos_x + advance_x + (player->tam / 2) - 1;
 	aux_y = player->pos_y + advance_y + (player->tam / 2) - 1;
-	
-	if(map->map[aux_y / map->height][aux_x / map->width] == '1')
-		return -1;
-	return 0;
+	if (map->map[aux_y / map->height][aux_x / map->width] == '1')
+		return (-1);
+	return (0);
 }
