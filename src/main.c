2b329e0e76d4bc868fefdd1781a5ff0c4e818d09
F N <fnieves-@c3r3s2.42malaga.com>
2023-03-28 23:56:16 +0200

    antes d eintegracion 1

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: fnieves- <fnieves-@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 18:43:48 by anramire          #+#    #+#             */
-/*   Updated: 2023/03/28 10:48:04 by fnieves-         ###   ########.fr       */
+/*   Updated: 2023/03/28 23:55:55 by fnieves-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,10 +25,7 @@ int	main(int argc, char** argv)
 	//(void) argv;
 	if (argc != 2)
 		return (print_error(ERRNBINP_MES, ERRNBINP));
-	atexit(check_leaks);
 	if (ft_parse(argv[1])) //PASAR LA ESTRUCTURA GENERAL DE ANDRES, previa inicialización
 		return (EXIT_FAILURE);
-	/* FElipe */
-	//init_window();
 	return (EXIT_SUCCESS);
 }
